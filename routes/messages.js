6dcbca50a1ab3e72e5c021ea02ac1a0dd11ef40f
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-05-25 17:46:45 -0400

    Add query parameters and delete functionality

--- routes/messages.js
+++ routes/messages.js
@@ -3,10 +3,32 @@ const router = express.Router();
 const messages = require('../db/messages.js')
 
 
-router.get("/", (req, res) => {
-    res.send(messages);
+router.get("/", (req, res, next) => {
+
+    if(req.query.userId) {
+        const newArr = []
+
+        messages.forEach(element => {
+            if(element.userId == req.query.userId) {
+                newArr.push(element)
+            }
+        })
+
+        if (newArr.length > 0) {
+            res.json(newArr);
+          } else {
+            res.send("Couldn't find that users messages")
+          }
+    } else {
+        res.send(messages)
+    }
+
+    
+    
+    
 })
 
+// At least one data category should allow for client creation via a POST request.
 router.post("/", (req, res) => {
     if (req.body.userId && req.body.sender && req.body.message) {
         const message = {
@@ -26,8 +48,8 @@ router.post("/", (req, res) => {
 
 })
 
+// At least one data category should allow for client manipulation via a PATCH or PUT request.
 router.patch("/:id", (req, res, next) => {
-    console.log("patch happen")
     const message = messages.find((m, i) => {
         if(m.id == req.params.id) {
             
@@ -45,5 +67,21 @@ router.patch("/:id", (req, res, next) => {
 })
 
 
+// At least one data category should allow for client deletion via a DELETE request.
+router.delete('/:id', (req, res, next) => {
+
+    const message = messages.find((m, i) => {
+        if(m.id == req.params.id) {
+            messages.splice(i, 1);
+            return true;
+        }
+    })
+
+    if(message) {
+        res.json(message);
+    } else {
+        next();
+    }
+})
 
 module.exports = router;
